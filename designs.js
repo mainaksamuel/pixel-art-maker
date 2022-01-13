35dc9ea3f2f107c4ecf87e275cd7a64017cf586e
Samuel Maina <mainasamkaranja@gmail.com>
2022-01-13 20:32:26 +0300

    Implemented project tweaks
    1. Removed event listener from the table and added it
       to the grid cells.This makses sure that if a user mistakenly
       clicks on the outside borders of the grid, the whole grid
       does not get colored at once.
    2. Added `max` attribute to the `height` and `width` input tags.
       This ensures that when the user inputs a very large number,
       the canvas does not get messed up and there's no hit on the
       processing power of the browser.
    3. Added code comments

--- designs.js
+++ designs.js
@@ -2,6 +2,7 @@
 const colorPicker = document.getElementById('colorPicker');
 let color = colorPicker.value;
 
+// Update the `color` variable whenever a user picks a different color.
 colorPicker.addEventListener('change', () => {
   color = colorPicker.value;
 });
@@ -15,24 +16,32 @@ form.addEventListener('submit', evt => {
 });
 
 
-function colorBox(target) {
+/*
+ * Set the background color of a cell to the chosen color
+ * params: target => HTMLElement
+ */
+function colorCell(target) {
   target.style.cssText = `background-color: ${color}`;
 }
 
 
+/*
+ * Draw a grid on the Pixel-Canvas
+ */
 function makeGrid() {
   const height = Number(document.getElementById('inputHeight').value);
   const width = Number(document.getElementById('inputWidth').value);
-
   const table = document.getElementById('pixelCanvas');
-  table.addEventListener('click', evt => colorBox(evt.target));
+
+  // Clear the table before drawing a new grid
   table.innerHTML = '';
 
+  // draw a `height` by `width` grid
   for (let w = 0; w < height; w++) {
-    let row = document.createElement('tr');
+    let row = table.insertRow();
     for (let h = 0; h < width; h++) {
-      row.appendChild(document.createElement('td'));
+      let cell = row.insertCell();
+      cell.addEventListener('click', evt => colorCell(evt.target));
     }
-    table.appendChild(row);
   }
 }
