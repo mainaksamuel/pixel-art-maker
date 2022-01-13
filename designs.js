9deb2d1bdc6e6a6df8af320a5aa032dd4fc6c783
Samuel Maina <mainasamkaranja@gmail.com>
2022-01-13 14:17:19 +0300

    First Commit

---
+++ designs.js
@@ -0,0 +1,37 @@
+// Select color input
+const colorPicker = document.getElementById('colorPicker');
+let color = colorPicker.value;
+
+colorPicker.addEventListener('change', () => {
+  color = colorPicker.value;
+  console.log(`Color: ${color}`);
+});
+
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+const form = document.getElementById('sizePicker');
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  makeGrid();
+});
+
+function colorBox(target) {
+  target.style.cssText = `background-color: ${color}`;
+}
+
+function makeGrid() {
+  const height = Number(document.getElementById('inputHeight').value);
+  const width = Number(document.getElementById('inputWidth').value);
+
+  const table = document.getElementById('pixelCanvas');
+  table.addEventListener('click', evt => colorBox(evt.target));
+  table.innerHTML = '';
+  for (let w = 0; w < height; w++) {
+    let row = document.createElement('tr');
+    for (let h = 0; h < width; h++) {
+      row.appendChild(document.createElement('td'));
+    }
+    table.appendChild(row);
+  }
+}
