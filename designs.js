4b81f0c4332a73f75b4436d7bab477720bda098f
Samuel Maina <mainasamkaranja@gmail.com>
2022-01-22 10:18:12 +0300

    Minor tweaks
    Changed:
    1. code block documentation
    2. variable declaration

--- designs.js
+++ designs.js
@@ -17,8 +17,8 @@ form.addEventListener('submit', evt => {
 
 
 /*
- * Set the background color of a cell to the chosen color.
- * Resets the cell's background color to '' when the cell is clicked, if
+ * Sets the background color of a cell to the chosen color, otherwise
+ * resets the cell's background color (to '') when the cell is clicked, if
  *    the background-color is the same as the color on the color picker
  *
  * params: target => HTMLElement
@@ -58,10 +58,10 @@ function makeGrid() {
   table.innerHTML = '';
 
   // Draw a `height` by `width` grid while adding `click` event-listener to the cells
-  for (let w = 0; w < height; w++) {
-    let row = table.insertRow();
-    for (let h = 0; h < width; h++) {
-      let cell = row.insertCell();
+  for (let h = 0; h < height; h++) {
+    const row = table.insertRow();
+    for (let w = 0; w < width; w++) {
+      const cell = row.insertCell();
       cell.addEventListener('click', evt => colorCell(evt.target));
     }
   }
